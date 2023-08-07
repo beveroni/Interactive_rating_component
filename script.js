a69c46e000528e37f91c0864710c89f7df566796
Veronika Bendova <svechovaver@gmail.com>
2023-08-07 20:28:08 +0200

    Task done1

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const submitButton = document.querySelector('.rating-btn');
-  const thankYou = document.querySelector('.thankyou-container');
-  const ratingForm = document.querySelector('.rating-form');
-  const selectedNumber = document.querySelector('.thankyou-num--chosen');
+const submitButton = document.querySelector('.rating-btn');
+const thankYou = document.querySelector('.thankyou-container');
+const ratingForm = document.querySelector('.rating-form');
+const selectedNumber = document.querySelector('.thankyou-num--chosen');
 
-  // posluchač události kliknutí na tlačítko Submit
-  submitButton.addEventListener('click', (event) => {
-    // zabránění výchozímu chování tlačítka submit
-    event.preventDefault();
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
 
-    // Hodnota vybraného inputu
-    const selectedInput = document.querySelector(
-      'input[name="number"]:checked',
-    );
-    const selectedValue = selectedInput.value;
+  const selectedInput = document.querySelector('input[name="number"]:checked');
+  const selectedValue = selectedInput.value;
 
-    // Nastavení textu hodnoty
-    selectedNumber.textContent = `You selected ${selectedValue} out of 5`;
+  selectedNumber.textContent = `You selected ${selectedValue} out of 5`;
 
-    // Skrytí formuláře a zobrazení thank you sekce
-    ratingForm.style.display = 'none';
-    thankYou.style.display = 'block';
-  });
+  ratingForm.style.display = 'none';
+  thankYou.style.display = 'block';
 });
